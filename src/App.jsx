5cf0673dbdf449f3c6ffcb90d7d4381fed314036
Miguel Lucena <miguelcenalu@gmail.com>
2024-08-11 19:36:37 -0300

    Formatting changes on post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-
+import { Post } from './Post'
 
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Post author="Diego Fernandes" 
+    content="Hello World"/>
       
       
   )
