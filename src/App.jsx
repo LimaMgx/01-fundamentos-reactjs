90c984fce524ff2d6431c0b8050c4ac96f2b2cfc
Miguel Lucena <miguelcenalu@gmail.com>
2024-08-31 22:40:33 -0300

    Sidebar's and app's sync configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { Post } from './Components/Post'
 import { Header } from './Components/Header'
+import { Sidebar } from './Components/Sidebar';
 
 import styles from './Components/App.module.css';
-
 import './global.css';
+
 
 function App() {
   return (
@@ -13,14 +14,24 @@ function App() {
         <Header 
           
         />
-        <Post 
-          author="Diego Fernandes" 
-          content="Hello World"
-        />
-        <Post 
-          author="Almirante Peralta"
-          content="Blalalalakjhsdhdhg"
-        />
+
+        <div className={styles.wrapper}>
+            <Sidebar>
+
+            </Sidebar>
+            <main>
+                <Post 
+                  author="Diego Fernandes" 
+                  content="Hello World"
+                />
+                <Post 
+                  author="Almirante Peralta"
+                  content="Blalalalakjhsdhdhg"
+                />
+            </main>
+
+        </div>
+        
         
       
       </div>  
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        
+        
+        <aside className={styles.sidebar}>
+                <img src="" alt="" />
+        
+        </aside>
+        
+
+       
+    )
+}
